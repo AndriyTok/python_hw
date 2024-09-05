8c8a7875724ce4308efc80113aa3ff263860ee3c
andriy_tokovyy <andriy.tokovyy@gmail.com>
2024-09-05 23:12:28 +0300

    copied from consultation

--- homework_2/task_4.py
+++ homework_2/task_4.py
@@ -1,2 +1,31 @@
 # 4) створити декоратор котрий буде підраховувати скільки разів була запущена функція продекорована цим декоратором,
 # та буде виводити це значення після виконання функцій
+
+def count_decor(func):
+    count = 0
+
+    def inner(*args, **kwargs):
+        nonlocal count
+        count += 1
+        func(*args, **kwargs)
+        print(f'{count=}')
+
+    return inner
+
+
+@count_decor
+def func1():
+    print('func1')
+
+
+@count_decor
+def func2():
+    print('func2')
+
+
+func1()
+func2()
+func1()
+func2()
+func1()
+func2()
