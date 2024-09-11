d921268907c4b0611806f7906225aa0d8453696a
andriy_tokovyy <andriy.tokovyy@gmail.com>
2024-09-11 15:39:10 +0300

    task_2 done

--- homework_3/task_2.py
+++ homework_3/task_2.py
@@ -5,3 +5,44 @@
 #
 # в класі попелюшки має бути count який буде зберігати кількість створених екземплярів классу
 # також має бути метод классу який буде виводити це значення
+from abc import ABC
+
+class Human(ABC):
+    def __init__(self, name, age):
+        self.name:str = name
+        self.age:int = age
+
+class Cinderella(Human):
+    count = 0 # variable for counting
+
+    def __init__(self, name, age, foot_size):
+        super().__init__(name, age)
+        self.foot_size:int = foot_size
+        Cinderella.count += 1 #increasing count
+
+    @classmethod
+    def get_count(cls):
+        return cls.count
+
+
+class Prince(Human):
+    def __init__(self, name, age, found_foot_size):
+        super().__init__(name, age)
+        self.found_foot_size:int = found_foot_size
+    def __find__(self, cinderellas_list):
+        for item in cinderellas_list:
+            if item.foot_size == self.found_foot_size:
+                print(f"{item.name} is a princess!")
+                break
+
+
+cinderellas:list[Cinderella] = [
+    Cinderella('Alla', 18, 38),
+    Cinderella('Nina', 19, 40),
+    Cinderella('Rosa', 20, 35),
+    Cinderella('Alisa', 30, 34),
+]
+
+prince = Prince('Alex', 35, 35)
+prince.__find__(cinderellas)
+print(Cinderella.count)
