cc5a4b03fb277381a383ee485da9543025df8c08
andriy_tokovyy <andriy.tokovyy@gmail.com>
2024-09-12 00:30:53 +0300

    task_3 done

--- homework_3/task_3.py
+++ homework_3/task_3.py
@@ -29,3 +29,70 @@
 #
 # isinstance(max, User) -> True
 # isinstance(shape, User) -> False
+
+from abc import ABC, abstractmethod
+from sympy.codegen import Print
+
+
+class Printable(ABC):
+   @abstractmethod
+   def __print__(self):
+       print(str(self.__dict__))
+
+class Book(Printable):
+    def __init__(self, name, number_of_pages, title):
+        self.name = name
+        self.number_of_pages = number_of_pages
+        self.title = title
+
+
+    def __print__(self):
+        print(str(self.__dict__))
+
+class Magazine(Printable):
+    def __init__(self, name, number_of_pages):
+        self.name = name
+        self.number_of_pages = number_of_pages
+
+
+    def __print__(self):
+        print(str(self.__dict__))
+
+class Main:
+    __printable_list:list[Printable] = []
+
+    @classmethod
+    def add(cls, item:Book|Magazine):
+          if isinstance(item, (Book, Magazine)):
+                cls.__printable_list.append(item)
+
+    @classmethod
+    def show_all_magazines(cls):
+        for item in cls.__printable_list:
+            if isinstance(item, Magazine):
+                item.__print__()
+            else:
+                pass
+    @classmethod
+    def show_all_books(cls):
+        for item in cls.__printable_list:
+            if isinstance(item, Book):
+                item.__print__()
+            else:
+                pass
+
+
+Main.add(Magazine('magazine1', 10))
+Main.add(Magazine('magazine4', 12))
+Main.add(Book('book1', 300, 'Book1'))
+Main.add(Book('book2', 500, 'Book2'))
+Main.add(Book('book3', 1000, 'Book3'))
+Main.add(Book('book4', 80, 'Book4'))
+Main.add(Magazine('magazine3', 15))
+
+Main.add(Magazine('magazine2', 2))
+Main.add('ddddddddddddddd')
+
+Main.show_all_magazines()
+print('********************************************')
+Main.show_all_books()
